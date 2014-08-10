16d93c4e043f338a3b26c59ea9724e1f35e5a06f
SeonMyungLim <brainlip@gmail.com>
2014-08-10 16:18:36 +0900

    git

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+
 var request = require('request');
-var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName="
-var stationName = '';
-var afterStationName = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ=="
+var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
+var stationName;
+var afterStationName = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
+var apiKey = "hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ==";
+
 app.get('/test', function(req, res){
     stationName = '월평동';
 
