7bce27c069ca2a0a1336c2eed805ac74bcb5cb58
SeonMyungLim <brainlip@gmail.com>
2014-08-11 10:00:00 +0900

    projectAdd design fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var request = require('request');
 var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
 var stationName;
-var afterStationName = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
+var queryOption = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
 var apiKey = "hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ==";
 
 app.get('/test', function(req, res){
@@ -13,9 +13,8 @@ app.get('/test', function(req, res){
     postRequest();
 
     function postRequest(){
-        var dest = apiUrl+stationName+afterStationName+apiKey;
-        console.log(dest);
-        request(apiUrl+stationName+afterStationName+apiKey, function(err, response, body){
+        var dest = apiUrl+stationName+queryOption+apiKey;
+        request(dest, function(err, response, body){
             if(!err && response.statusCode == 200){
                 console.log(body);
             }else   console.log(err);
