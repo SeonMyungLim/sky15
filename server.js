3dc98485b3c79a200aedf37d1bbd96ae94b5f86f
SeonMyungLim <brainlip@gmail.com>
2014-09-21 15:24:48 +0900

    commit

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var app = express();
 var request = require('request');
 
 
-//variables for api request 
+//variables for api request
 var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
 var stationName;
 var queryOption = "&dataTerm=daily&pageNo=1&numOfRows=10&ServiceKey=";
 var apiKey = "hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ==";
 
-app.get('/getAir/:station', function(req, res){
-    console.log(stationName = req.param('station'));
 //http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=평리동&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=서비스키
 //var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
 var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoIngireSvc/getMsrstnList?umdName=";
@@ -31,7 +29,7 @@ app.get('/test', function(req, res){
         request(dest, function(err, response, body){
             if (!err && response.statusCode == 200){
                 console.log(body);
-		res.jsonp(body);
+                res.jsonp(body);
             } else {
                 cosole.log(response);
                 console.log(err);
