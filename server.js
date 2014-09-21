ca15a1ef8062de1115adeded2e99a21459091789
SeonMyungLim <brainlip@gmail.com>
2014-09-21 15:54:08 +0900

    sadf

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/getAirCond', function(req, res){
 
         request(dest, function(err, response, body){
             if (!err && response.statusCode == 200){
-                var items = body.items[0];
-                var firstItem = items.item[0];
+                var items = body.children[0];
+                var firstItem = items.children[0];
                 tmX = firstItem.tmX;
                 tmY = firstItem.tmY;
                 console.log(tmX);
