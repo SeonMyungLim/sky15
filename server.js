3458006a7bc19202ffd18c40860576d8c24edcb1
SeonMyungLim <brainlip@gmail.com>
2014-09-21 15:15:29 +0900

    test

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ var app = express();
 
 var request = require('request');
 //http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=평리동&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=서비스키
-var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
+//var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
+var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoIngireSvc/getMsrstnList?umdName=";
 var stationName;
-var queryOption = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
+//var queryOption = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
+var queryOption = "&pageNo=1&numOfRows=10&ServiceKey";
 var apiKey = "hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ==";
 
-app.get('/test', function(req, res){
-    stationName = '평리동';
+var tmX, tmY;
 
+app.get('/test', function(req, res){
+    stationName = '혜화동';
     postRequest();
 
     function postRequest(){
         var dest = apiUrl+stationName+queryOption+apiKey;
         console.log(dest);
         request(dest, function(err, response, body){
-            if(!err && response.statusCode == 200){
+            if (!err && response.statusCode == 200){
                 console.log(body);
-            }else {
+            } else {
                 cosole.log(response);
                 console.log(err);
             }
@@ -28,6 +31,27 @@ app.get('/test', function(req, res){
 
 
 });
+
+//app.get('/test', function(req, res){
+    //stationName = '평리동';
+
+    //postRequest();
+
+    //function postRequest(){
+        //var dest = apiUrl+stationName+queryOption+apiKey;
+        //console.log(dest);
+        //request(dest, function(err, response, body){
+            //if (!err && response.statusCode == 200){
+                //console.log(body);
+            //} else {
+                //cosole.log(response);
+                //console.log(err);
+            //}
+        //});
+    //}
+
+
+//});
 
 
 var server = app.listen(3000, function(){
