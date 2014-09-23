ddfa5850d11997df336f6349692420fb15d13e16
SeonMyungLim <brainlip@gmail.com>
2014-09-23 10:58:57 +0900

    dfaf

--- server.js
+++ server.js
@@ -3,34 +3,38 @@ var app = express();
 var request = require('request');
 
 
-//variables for api request
-//http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=평리동&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=서비스키
-//var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
-//var queryOption = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
-var apiUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getTMStdrCrdnt?umdName=";
-var queryOption = "&pageNo=1&numOfRows=10&ServiceKey=";
+var apiUrlForTM = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getTMStdrCrdnt?umdName=";
+var queryOptionForTM = "&pageNo=1&numOfRows=10&ServiceKey=";
+
+var apiUrlForStation = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?";
+var queryOptionForStation = "&pageNo=1&numOfRows=10&ServiceKey=";
+
+var apiUrlForCond = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=";
+var queryOptionForCond = "&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=";
+
 var apiKey = "hAkyrmVgIwySVYQbyM36EpnB6R3ZOzxPkjakEQNbHEoGvsVn0X7dAmgo3DiQ/FPP11orgBEKRc5PLX8P4ZrURQ==";
 
 var stationName;
 var tmX, tmY;
 
-app.get('/getAirCond', function(req, res){
-    stationName = '신성동';
-    postRequest();
-    console.log(req.query);
 
+
+app.get('/getAirCond', function(req, res, next){
+    var conds = req.query;
+    var sidong = conds.sidong;
+    postRequest();
     function postRequest(){
-        var dest = apiUrl+stationName+queryOption+apiKey;
+        var dest = apiUrlForTM+sidong+queryOptionForTM+apiKey;
 
         request(dest, function(err, response, body){
             if (!err && response.statusCode == 200){
-                //var items = body.getElementsByTagName('items')[0];
-                //var firstItem = items.getElementsByTagName('item')[0];
-                console.log(body.children);
-                tmX = firstItem.tmX;
-                tmY = firstItem.tmY;
-                console.log(tmX);
-                res.jsonp(body);
+		var xml_x = new RegExp("<tmX>(.+?)<\/tmX>", "m");
+		var xml_y = new RegExp("<tmY>(.+?)<\/tmY>", "m");
+		var tmXTemp = body.match(xml_x);
+		var tmYTemp = body.match(xml_y);
+		tmX = tmXTemp[1];
+		tmY = tmYTemp[1];
+		next();
             } else {
                 cosole.log(response);
                 console.log(err);
@@ -41,27 +45,42 @@ app.get('/getAirCond', function(req, res){
 
 });
 
-//app.get('/test', function(req, res){
-    //stationName = '평리동';
+app.get('/getAirCond', function(req, res, next){
+    postRequest();
 
-    //postRequest();
+    function postRequest(){
+        var dest = apiUrlForStation+'tmX='+tmX+'&tmY='+tmY+queryOptionForStation+apiKey;
 
-    //function postRequest(){
-        //var dest = apiUrl+stationName+queryOption+apiKey;
-        //console.log(dest);
-        //request(dest, function(err, response, body){
-            //if (!err && response.statusCode == 200){
-                //console.log(body);
-            //} else {
-                //cosole.log(response);
-                //console.log(err);
-            //}
-        //});
-    //}
+        request(dest, function(err, response, body){
+            if (!err && response.statusCode == 200){
+		var xml_station = new RegExp("<stationName>(.+?)<\/stationName>", "m");
+		stationName = body.match(xml_station)[1];
+		next();
+            } else {
+                cosole.log(response);
+                console.log(err);
+            }
+        });
+    }
+});
 
+app.get('/getAirCond', function(req, res, next){
+    postRequest();
 
-//});
+    function postRequest(){
+        var dest = apiUrlForCond + stationName + queryOptionForCond + apiKey;
 
+        request(dest, function(err, response, body){
+            if (!err && response.statusCode == 200){
+		res.send(body);
+            } else {
+                cosole.log(response);
+                console.log(err);
+            }
+        });
+    }
+
+});
 
 var server = app.listen(3000, function(){
     console.log('listening on port %d', server.address().port);
