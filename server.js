58d73f2d554cefe3723f166b71b89ba827b779df
SeonMyungLim <brainlip@gmail.com>
2014-09-25 10:29:56 +0900

    asdf

--- server.js
+++ server.js
@@ -74,6 +74,9 @@ app.get('/getAirCond', function(req, res, next){
 
         request(dest, function(err, response, body){
             if (!err && response.statusCode == 200){
+                for (var key in body) {
+                    console.log(body[key]);
+                }
                 res.send(body);
             } else {
                 cosole.log(response);
